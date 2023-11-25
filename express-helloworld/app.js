60c6688827f94e1f1e66ecac1a62c17f987450af
pillinikitha <pillinikitha344@gmail.com>
2023-11-25 12:07:05 +0530

    add/star endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,11 +1,16 @@
 var express = require('express');
 app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+app.get('/',function(req,res){
+  res.send('Hello World!\n ');
+});
+
+app.get('/star', function (req, res) {
+  res.send('Rebel Star!\n');
 });
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
+
 
